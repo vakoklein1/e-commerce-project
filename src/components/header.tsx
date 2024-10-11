5414666d8f02288d76db4f08567dfc01462acdbf
anakapanadzeklein <anakapanadzeklein@gmail.com>
2024-10-12 02:42:57 +0400

    added second section to homepage and dropdown menu to Categories in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,10 @@ const Header: React.FC = () => {
   return (
     <div>
 
-      <div className='w-full h-10 text-white text-sm flex items-center justify-center' style={{ backgroundColor: '#0E1422', fontFamily: 'inter' }}>
-        <span className='font-light'>Get 25% OFF on your first order.</span>
-        <span className='ml-2 cursor-pointer'>Order Now</span>
-      </div>
+        <div className='w-full h-12 text-white text-sm flex items-center justify-center' style={{ backgroundColor: '#0E1422', fontFamily: 'inter' }}>
+          <span className='font-light'>Get 25% OFF on your first order.</span>
+          <span className='ml-2 cursor-pointer underline'>Order Now</span>
+        </div>
 
       
     <header className="bg-white w-full h-20 flex items-center justify-center space-x-36" style={{ color: '#5C5F6A' }}>
@@ -18,14 +18,50 @@ const Header: React.FC = () => {
 
       <nav>
         <ul className="flex space-x-8">
+          
           <li><Link to="/">Home</Link></li>
 
-          <li className='flex items-center'> {}
-            <Link to="/categories" className="flex items-center space-x-1"> {}
-              <span>Categories</span>
-              <img src="/images/Vector.png" alt="Logo" className="pl-2" /> {}
-            </Link>
+
+          <li className='relative group'>
+                  <Link to="/categories" className="flex items-center space-x-1">
+                    <span>Categories</span>
+                    <img src="/images/Vector.png" alt="Dropdown" className="pl-2" />
+                  </Link>
+
+            <div className="absolute hidden group-hover:block bg-white shadow-lg">
+              <div className="p-2 flex space-x-8">
+
+                <div>
+                  <h1 className='text-black font-semibold mb-2 mx-4 mt-2 cursor-pointer'>Female</h1>
+                  <ul>
+                    <li className="block px-4 py-2 cursor-pointer">Tops</li>
+                    <li className="block px-4 py-2 cursor-pointer">Bottoms</li>
+                    <li className="block px-4 py-2 cursor-pointer">Dresses</li>
+                    <li className="block px-4 py-2 cursor-pointer">Two-Piece</li>
+                    <li className="block px-4 py-2 cursor-pointer">Jackets + Coats</li>
+                    <li className="block px-4 py-2 cursor-pointer">Sportswear</li>
+                    <li className="block px-4 py-2 cursor-pointer">Swimwear</li>
+                    <li className="block px-4 py-2 cursor-pointer">Shoes</li>
+                  </ul>
+                </div>
+
+
+                <div>
+                  <h1 className='text-black font-semibold mb-2 mx-4 mt-2 cursor-pointer'>Male</h1>
+                  <ul>
+                    <li className="block px-4 py-2 cursor-pointer">Tops</li>
+                    <li className="block px-4 py-2 cursor-pointer">Bottoms</li>
+                    <li className="block px-4 py-2 cursor-pointer">Two-Piece</li>
+                    <li className="block px-4 py-2 cursor-pointer">Jackets + Coats</li>
+                    <li className="block px-4 py-2 cursor-pointer">Sportswear</li>
+                    <li className="block px-4 py-2 cursor-pointer">Swimwear</li>
+                    <li className="block px-4 py-2 cursor-pointer">Shoes</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </li>
+
 
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,23 +4,44 @@ import '../App.css';
 const Home: React.FC = () => {
   return (
     <main>
-      <section className='w-full flex items-center justify-center gap-96' style={{ backgroundColor: '#F6F6F6', height: '440px' }}>
-
-
+      <section className='w-full flex items-center justify-center gap-96' style={{ backgroundColor: '#F6F6F6', height: '440px', maxWidth: '1440px', margin: '0 auto' }}>
         <div className='flex flex-col'>
-
           <h1 className='text-3xl font-semibold pl-0.5' style={{ color: '#202533' }}> Fresh Arrivals Online </h1>
           <p className='text-sm font-normal mt-4' style={{ color: '#474B57' }}> Discover Our Newest Collection Today. </p>
-        
-        <button className='text-white text-sm flex items-center justify-center rounded mt-12' style={{ backgroundColor: '#0E1422', width: '183px', height: '44px'}}>
-          <span> View Collection </span>
-          <img src="/images/ArrowRight.png" alt="right-arrow" className='pl-3' />
-        </button>
-
+          
+          <button className='text-white text-sm flex items-center justify-center rounded mt-12' style={{ backgroundColor: '#0E1422', width: '183px', height: '44px' }}>
+            <span> View Collection </span>
+            <img src="/images/ArrowRight.png" alt="right-arrow" className='pl-3' />
+          </button>
         </div>
 
+        <img src="/images/Man.png" alt="man" style={{ width: '340px', height: '340px', marginTop: '98px' }} />
+      </section>
+ 
+      <section className='w-full flex items-center justify-center gap-16' style={{ backgroundColor: '#FFF', height: '440px', maxWidth: '1440px', margin: '0 auto' }}>
+        <div className="max-w-sm p-6 bg-white mt-40 mb-40" style={{ height: '218px' }}>
+            <img src="/images/HomeCardOne.png" alt="home-card-one" />
+            <h1 className='font-semibold text-base mt-6'>Free Shipping</h1>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 mt-4">
+              Upgrade your style today and get FREE shipping on all orders! Don't miss out.
+            </p>
+        </div>
 
-        <img src="/images/Man.png" alt="man" style={{ width: '340px', height: '340px', marginTop: '98px' }}/>
+        <div className="max-w-sm p-6 bg-white mt-40 mb-40" style={{ height: '218px' }}>
+            <img src="/images/HomeCardTwo.png" alt="home-card-two" />
+            <h1 className='font-semibold text-base mt-6'>Satisfaction Guarantee</h1>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 mt-4">
+            Shop confidently with our Satisfaction Guarantee: Love it or get a refund.
+            </p>
+        </div>
+
+        <div className="max-w-sm p-6 bg-white mt-40 mb-40" style={{ height: '218px' }}>
+            <img src="/images/HomeCardThree.png" alt="home-card-three" />
+            <h1 className='font-semibold text-base mt-6'>Secure Payment</h1>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400 mt-4">
+            Your security is our priority. Your payments are secure with us.
+            </p>
+        </div>
       </section>
     </main>
   );
